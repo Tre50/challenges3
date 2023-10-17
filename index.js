95888b8309ad7c419826c8255b5b8ce1e467718d
Tremayne Johnson <tremayne4@gmail.com>
2023-10-17 18:44:04 -0400

    challenge3

--- index.js
+++ index.js
@@ -2,21 +2,20 @@ import express from 'express'
 
 let user = ['tim', 'tom', 'terry']
 
-const express = express()
+const app = express()
 const port = 3000
 
-app.get ('/usewr/:name', (req, res)=>{
+app.get ('/user/:name', (req, res)=>{
     const user = req.params
-    res.send ('createnew student name, ${user.name}')
+    res.send ('create new student name, ${user,name}')
 })
+//app.post('/:students', (req, res) => {
+  //  res.send('POST request to the homepage')
+  //})
    
-    app.listen(port,()=>{
-        console.log('listening on http://localhost:${port}')
+    app.listen(port, () => {
+        console.log('Example app listening on port http://localhost:${port}')
     })
 
 
 
-
-//app.post('/:students', (req, res) => {
-  //  res.send('POST request to the homepage')
-  //})
